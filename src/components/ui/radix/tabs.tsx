f1b160565adb8bc4c32e8ab41de7e090ca539519
melwin <114172287+itzTedx@users.noreply.github.com>
2025-10-25 11:12:29 +0400

    refactor(components, tabs): update TabsTrigger component to use w-fit for improved layout consistency

--- src/components/ui/radix/tabs.tsx
+++ src/components/ui/radix/tabs.tsx
@@ -41,10 +41,10 @@ type TabsTriggerProps = TabsTriggerPrimitiveProps;
 
 function TabsTrigger({ className, ...props }: TabsTriggerProps) {
   return (
-    <TabsHighlightItemPrimitive className="flex-1" value={props.value}>
+    <TabsHighlightItemPrimitive className="w-fit" value={props.value}>
       <TabsTriggerPrimitive
         className={cn(
-          "inline-flex h-[calc(100%-1px)] w-full flex-1 items-center justify-center gap-1.5 whitespace-nowrap rounded-md border bg-stone-alpha-10/50 px-2 py-1 font-medium text-muted-foreground text-sm transition-colors duration-500 ease-in-out focus-visible:border-ring focus-visible:outline-1 focus-visible:outline-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+          "inline-flex h-[calc(100%-1px)] w-fit flex-1 items-center justify-center gap-1.5 whitespace-nowrap rounded-md border bg-stone-alpha-10/50 px-2 py-1 font-medium text-muted-foreground text-sm transition-colors duration-500 ease-in-out focus-visible:border-ring focus-visible:outline-1 focus-visible:outline-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground [&_svg:not([class*='size-'])]:size-4 [&_svg]:pointer-events-none [&_svg]:shrink-0",
           // Active State
           "data-[state=active]:border-transparent data-[state=active]:text-primary-900 data-[state=active]:[&_div]:bg-primary-300 data-[state=active]:[&_svg]:text-primary-950",
           className
