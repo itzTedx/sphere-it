8f815c533ca3011a479272d3edb71bbb0ad2e911
melwin <114172287+itzTedx@users.noreply.github.com>
2025-10-25 12:51:58 +0400

    feat(auth): add LinkedIn social authentication support with environment variables

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -3,12 +3,20 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { db } from "@/server";
 
+import { env } from "../env/server";
+
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
     usePlural: true,
   }),
   appName: "Sphere IT Global",
+  socialProviders: {
+    linkedin: {
+      clientId: env.LINKEDIN_CLIENT_ID,
+      clientSecret: env.LINKEDIN_CLIENT_SECRET,
+    },
+  },
   advanced: {
     cookiePrefix: "sphere-it",
     database: {
--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -5,6 +5,10 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.url(),
     DB_FILE_NAME: z.url(),
+
+    // LINKEDIN CREDENTIALS
+    LINKEDIN_CLIENT_ID: z.string().min(1),
+    LINKEDIN_CLIENT_SECRET: z.string().min(1),
   },
   runtimeEnv: process.env,
 });
