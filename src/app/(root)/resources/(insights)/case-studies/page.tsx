d5712a18054232fbbede583bfe18abf5ef230f10
melwin <114172287+itzTedx@users.noreply.github.com>
2025-11-10 12:35:28 +0400

    feat(case-studies): add Load More button and Call to Action in Case Studies page

--- src/app/(root)/resources/(insights)/case-studies/page.tsx
+++ src/app/(root)/resources/(insights)/case-studies/page.tsx
@@ -1,11 +1,30 @@
+import { Cta } from "@/components/layout/cta";
+import { Button } from "@/components/ui/button";
 import { TabsContent } from "@/components/ui/tabs";
+
+import { IconChevronDown } from "@/assets/icons";
 
 import { InsightsLayout } from "../components/insights-layout";
 
 export default function CaseStudiesPage() {
   return (
     <InsightsLayout>
-      <TabsContent value="/resources/case-studies">CaseStudiesPage</TabsContent>
+      <TabsContent value="/resources/case-studies">
+        <div className="container max-w-7xl">
+          <main className="mb-12">
+            <article className="grid grid-cols-3 gap-4 py-6">Case Studies</article>
+            <div className="flex items-center justify-center">
+              <Button variant="ghost">
+                Load More{" "}
+                <span className="flex size-8 shrink-0 items-center justify-center rounded-full bg-stone-300 transition-colors">
+                  <IconChevronDown className="text-stone-500" />
+                </span>
+              </Button>
+            </div>
+          </main>
+        </div>
+        <Cta />
+      </TabsContent>
     </InsightsLayout>
   );
 }
--- src/app/(root)/resources/(insights)/research-papers/page.tsx
+++ src/app/(root)/resources/(insights)/research-papers/page.tsx
@@ -1,11 +1,29 @@
+import { Cta } from "@/components/layout/cta";
+import { Button } from "@/components/ui/button";
 import { TabsContent } from "@/components/ui/tabs";
 
-import { InsightsLayout } from "../components/insights-layout";
+import { IconChevronDown } from "@/assets/icons";
 
+import { InsightsLayout } from "../components/insights-layout";
 export default function ResearchPaperPage() {
   return (
     <InsightsLayout>
-      <TabsContent value="/resources/research-papers">ResearchPaperPage</TabsContent>
+      <TabsContent value="/resources/research-papers">
+        <div className="container max-w-7xl">
+          <main className="mb-12">
+            <article className="grid grid-cols-3 gap-4 py-6">Research Papers</article>
+            <div className="flex items-center justify-center">
+              <Button variant="ghost">
+                Load More{" "}
+                <span className="flex size-8 shrink-0 items-center justify-center rounded-full bg-stone-300 transition-colors">
+                  <IconChevronDown className="text-stone-500" />
+                </span>
+              </Button>
+            </div>
+          </main>
+        </div>
+        <Cta />
+      </TabsContent>
     </InsightsLayout>
   );
 }
